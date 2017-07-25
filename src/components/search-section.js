45f9e5862c696f302389a3aafefbcc5e1b515a4c
desaree <srhboo@gmail.com>
2017-07-25 12:20:36 -0400

    revert some changes

--- src/components/search-section.js
+++ src/components/search-section.js
@@ -19,8 +19,10 @@ export const SearchSection = ({input,
                                ingredientsChange}) => (
   <div className="search-section">
     <div className="search-left">
+      <span className="keep-together">
         <input value={input} onChange={ev => updateInput(ev.target.value)} /> 
-        <button className="search-button" onClick={ev => search(input)}>Get Cooking!</button>
+        < button className="search-button" onClick={ev => search(input)}>Get Cooking!</button>
+      </span>
     </div>
     <div className="advanced-options">
         <div className="options-label" onClick={ev => displayOptions(!showOptions)}> 
