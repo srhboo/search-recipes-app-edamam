f6e9fed2afad18579fd1edd27b5129fe25382402
desaree <srhboo@gmail.com>
2017-07-27 11:49:25 -0400

    adjusted label options for edamam developer account

--- src/constants/options.js
+++ src/constants/options.js
@@ -1,8 +1,11 @@
 export const calIndex = { min:0, max: 1};
 export const labelIndex = {diet: 0, health:1};
 
-export const dietLabels = ["balanced","high-protein","high-fiber","low-carb","low-fat"];
-export const healthLabels = ["dairy-free","egg-free","fish-free","gluten-free","kidney-friendly","kosher","low-sugar","paleo","peanut-free","pecatarian","pork-free","shellfish-free","soy-free","vegan","vegetarian"];
+export const dietLabels = ["balanced","high-protein","low-carb","low-fat"];
+export const healthLabels = ["peanut-free","vegan","vegetarian","sugar-conscious","tree-nut-free","alcohol-free"];
 export const allLabels = ["all", ...dietLabels, ...healthLabels];
 
 export const defaultAny = val => val.length ? val : "any";
+
+
+
