59784d0c5843c1bbc49a395670fc3fb58f13b66f
desaree <srhboo@gmail.com>
2017-07-30 23:58:27 -0400

    clean up

--- src/components/Mobile/mobile-search-box.js
+++ src/components/Mobile/mobile-search-box.js
@@ -5,6 +5,6 @@ export const MobileSearchBox = props => (
     <div>
         <input value={props.input} placeholder="Search Recipes" onChange={ev => props.updateInput(ev.target.value)} /> 
         <OptionsBox {...props} screen="mobile" />
-        <a href="#results-anchor"><button className="search-button" onClick={ev => props.search(props.input, props.options)}>Get Cooking!</button></a>
+        <button className="search-button" onClick={ev => props.search(props.input, props.options)}>Get Cooking!</button>
     </div>
 );
--- src/components/Mobile/mobile-search-section.js
+++ src/components/Mobile/mobile-search-section.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { MobileSearchBox } from './mobile-search-box';
 
 export const MobileSearchSection = props => (
-  <div className="search-section mobile" onLoad={console.log("loaded")}>
+  <div className="search-section mobile">
       <MobileSearchBox screen="mobile" {...props} />
   </div>
 );
