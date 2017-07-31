b0e9d2a9e9050a3785891a7971e34bc5ece5546b
desaree <srhboo@gmail.com>
2017-07-31 13:21:30 -0400

    add logo

--- src/components/Header/search-section.js
+++ src/components/Header/search-section.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { SearchBox } from './search-box';
 import { AdvancedOptions } from './advanced-options';
+import logo from '../../img/logo.svg';
 
 export const SearchSection = props => (
   <div className="search-section desktop">
@@ -9,7 +10,7 @@ export const SearchSection = props => (
       <AdvancedOptions {...props} />
     </div>
     <div className="search-right">
-      <div className="app-title">find-a-recipe</div>
+      <div className="app-title"><img src={logo}/>find-a-recipe</div>
     </div>  
   </div>
 );
--- src/components/Mobile/mobile-search-section.js
+++ src/components/Mobile/mobile-search-section.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { MobileSearchBox } from './mobile-search-box';
+import logo from '../../img/logo.svg';
 
 export const MobileSearchSection = props => (
   <div className="search-section mobile">
+      <img src={logo} />
       <MobileSearchBox screen="mobile" {...props} />
   </div>
 );
